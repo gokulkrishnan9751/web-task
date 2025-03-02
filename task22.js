c239462cbca6a0721378b7d88eef0e9193f3c5d3
gokul <ashketchum9751@gmail.com>
2025-03-02 22:12:20 +0530

    Removed all files from the repository

--- task22.js
+++
@@ -1,8 +0,0 @@
-function fun(){
-    document.getElementById('one').innerHTML = "Screen With : " +screen.width;
-    document.getElementById('two').innerHTML = "Screen With : " +screen.height;
-    document.getElementById('three').innerHTML = "Screen availwidth :" +screen.availWidth;
-    document.getElementById('four').innerHTML = "Screen availHeight :" +screen.availHeight;
-    document.getElementById('five').innerHTML = "Screen pixel depth :" +screen.pixelDepth;
-    document.getElementById('six').innerHTML =  "Screen colordepth :" +screen.colorDepth;
-}
